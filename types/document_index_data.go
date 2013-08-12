f75668d3fdfb1acc9fa72cef0ef1954608798e35
Hui Chen <unmerged@gmail.com>
2013-08-12 10:14:03 -0700

    Update document_index_data.go

--- types/document_index_data.go
+++ types/document_index_data.go
@@ -1,10 +1,10 @@
 package types
 
 type DocumentIndexData struct {
-	// 文档全文，用于生成待索引的关键词
+	// 文档全文（必须是UTF-8格式），用于生成待索引的关键词
 	Content string
 
-	// 文档标签，比如文档的类别属性等，这些标签并不出现在文档文本中
+	// 文档标签（必须是UTF-8格式），比如文档的类别属性等，这些标签并不出现在文档文本中
 	Labels []string
 
 	// 文档的评分字段，可以接纳任何类型的结构体
